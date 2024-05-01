52cc76a818fead0ca452e3de23e653d4b59b10df
alisaruman <ali_heidary007@yahoo.com>
2024-05-01 20:55:08 +0330

    fix caching strategy for issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -37,6 +37,7 @@ const issueForm = ({ issue }: { issue?: Issue }) => {
             if (issue) await axios.patch("/api/issues/" + issue.id, data);
             else await axios.post("/api/issues/", data);
             router.push("/issues");
+            router.refresh();
         } catch (error) {
             setIsSubmitting(false);
             setError("An error has occurred.");
