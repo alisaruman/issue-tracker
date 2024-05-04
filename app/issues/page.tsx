9f8f40487a2090a7281df65d01981081c3148909
alisaruman <ali_heidary007@yahoo.com>
2024-05-04 21:23:06 +0330

    delete front handler

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,10 @@
 import { IssueStatusBadge, Link } from "@/app/components/index";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
     const issues = await prisma.issue.findMany();
-    await delay(2000);
 
     return (
         <div>
