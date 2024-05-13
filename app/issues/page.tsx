ca1faaece02b0ccdb9f28b6f04cc5be44f85dc3c
alisaruman <ali_heidary007@yahoo.com>
2024-05-13 21:43:19 +0330

    refactor issue table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,25 +1,12 @@
-import { IssueStatusBadge, Link } from "@/app/components/index";
 import prisma from "@/prisma/client";
-import { Table } from "@radix-ui/themes";
-import IssueActions from "./list/IssueActions";
-import { Issue, Status } from "@prisma/client";
-import { default as NextLink } from "next/link";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { Status } from "@prisma/client";
 import Pagination from "../components/Pagination";
+import IssueActions from "./list/IssueActions";
+import IssueTable, { IssueQuery, columnNames } from "./list/IssueTable";
 
 interface Props {
-    searchParams: {
-        status: Status;
-        orderBy: keyof Issue;
-        page: string;
-    };
+    searchParams: IssueQuery;
 }
-
-const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: "Issue", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-];
 
 const IssuesPage = async ({ searchParams }: Props) => {
     const statuses = Object.values(Status);
@@ -28,9 +15,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
         : undefined;
     const where = { status };
 
-    const orderBy = columns
-        .map((column) => column.value)
-        .includes(searchParams.orderBy)
+    const orderBy = columnNames.includes(searchParams.orderBy)
         ? { [searchParams.orderBy]: "asc" }
         : undefined;
 
@@ -49,52 +34,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
     return (
         <div>
             <IssueActions />
-            <Table.Root className="rt-variant-surface">
-                <Table.Header>
-                    <Table.Row>
-                        {columns.map((column) => (
-                            <Table.ColumnHeaderCell
-                                key={column.value}
-                                className={column.className}
-                            >
-                                <NextLink
-                                    href={{
-                                        query: {
-                                            ...searchParams,
-                                            orderBy: column.value,
-                                        },
-                                    }}
-                                >
-                                    {column.label}
-                                </NextLink>
-                                {column.value === searchParams.orderBy && (
-                                    <ArrowUpIcon className="inline" />
-                                )}
-                            </Table.ColumnHeaderCell>
-                        ))}
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {issues.map((issue) => (
-                        <Table.Row key={issue.id}>
-                            <Table.Cell>
-                                <Link href={`/issues/${issue.id}`}>
-                                    {issue.title}
-                                </Link>
-                                <div className="md:hidden">
-                                    <IssueStatusBadge status={issue.status} />
-                                </div>
-                            </Table.Cell>
-                            <Table.Cell className="hidden md:table-cell">
-                                <IssueStatusBadge status={issue.status} />
-                            </Table.Cell>
-                            <Table.Cell className="hidden md:table-cell">
-                                {issue.createdAt.toDateString()}
-                            </Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table.Root>
+            <IssueTable issues={issues} searchParams={searchParams} />
             <Pagination
                 currentPage={page}
                 pageSize={pageSize}
